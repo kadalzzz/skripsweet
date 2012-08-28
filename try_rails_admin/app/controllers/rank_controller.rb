1bbc8b5dc3d1539dc984459802ed3bb14cedb535
kadalzzz <yak.3003@gmail.com>
2012-08-29 01:19:36 +0700

    fix error in view, but still need improvement

--- try_rails_admin/app/controllers/rank_controller.rb
+++ try_rails_admin/app/controllers/rank_controller.rb
@@ -1,6 +1,7 @@
 class RankController < ApplicationController
   def index
     @products = Product.find(:all, :order => "total_score DESC")
+    @value = AhpAfter.first
   end
 
 end
--- try_rails_admin/app/models/ahp_before.rb
+++ try_rails_admin/app/models/ahp_before.rb
@@ -1,2 +1,9 @@
 class AhpBefore < ActiveRecord::Base
+  after_create :run_ahp
+  after_update :run_ahp
+  
+  def run_ahp
+    Algorithm.ahp_priority
+  end
+
 end
